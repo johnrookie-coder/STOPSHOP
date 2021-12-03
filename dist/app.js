a57d5a5932dcaf99dd9d577f2dc63e5722ea19d3
John Andrey Galingan <johngalingan05@gmail.com>
2021-12-02 19:46:06 -0800

    Minor updates to "add to cart" functionality and add initial js file.

---
+++ dist/app.js
@@ -0,0 +1,179 @@
+"use strict";
+// in-use
+const cards = document.querySelectorAll(".get-started__row");
+const addToCart = document.querySelectorAll(".btn--prod");
+
+// Cards open new HTML page
+cards.forEach((card, i) => {
+  card.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (i === 0) {
+      window.open("product.html");
+    }
+    if (i === 1) {
+      window.open("delivery.html");
+    }
+
+    if (i === 2) {
+      window.open("payment.html");
+    }
+  });
+});
+
+//  ***************************** Add to Cart ***********************
+(function () {
+  addToCart.forEach((cart) => {
+    cart.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      if (e.target.closest(".btn--prod")) {
+        // Get the image location based where the click happens
+        let imgPath =
+          e.target.parentElement.parentElement.parentElement
+            .previousElementSibling.firstElementChild.src;
+
+        let imgPos = imgPath.indexOf("img") + 3;
+        let partialPath = imgPath.slice(imgPos);
+
+        // Get the productName
+        let productName =
+          e.target.parentElement.parentElement.children[0].textContent
+            .trim()
+            .toUpperCase();
+
+        // Get the price of the product
+        let price = e.target.parentElement.parentElement.children[2].textContent
+          .trim()
+          .toUpperCase();
+
+        // Make the sure the string is converted into a number
+        let finalPrice = +price.slice(3).trim().replace(",", "");
+
+        // Get the number of quantity
+        let quantity = +e.target.parentElement.children[0].children[1].value;
+
+        const item = {};
+
+        // Adds properties to the empty item object
+        item.name = productName;
+        item.price = finalPrice;
+        item.quantity = quantity;
+        item.img = `img${partialPath}`;
+
+        console.log(item);
+
+        // layout foreach item
+        const cartContainer = document.querySelector(".cart__details");
+        const cartItem = document.createElement("div");
+
+        // const cartTotal = document.querySelector(".cart__details__total");
+
+        cartItem.classList.add("cart__details__prod", "mb-xl");
+
+        cartItem.innerHTML = `
+          <div class="cart__details__img">
+            <img src="${item.img}" alt="${item.name}" />
+          </div>
+          <div class="cart__details__contents">
+            <div class="col-1">
+              <p>${item.name}</p>
+              <p>
+                <strong class="cart__details__price">${item.price.toFixed(
+                  2
+                )}</strong>
+              </p>
+            </div>
+            <div class="col-2">
+              <input type="number" class="cart__details__input" value="${
+                item.quantity
+              }" />
+              <button class="btn btn--xs">Remove</button>
+            </div>
+          </div>
+        `;
+
+        cartContainer.prepend(cartItem);
+        showTotals();
+      }
+    });
+  });
+})();
+
+// show totals
+function showTotals() {
+  const total = [];
+  const quantities = [];
+
+  const items = document.querySelectorAll(".cart__details__price");
+  const qty = document.querySelectorAll(".cart__details__input");
+
+  qty.forEach((quantity) => {
+    return quantities.push(quantity.value);
+  });
+
+  items.forEach((item, i) => {
+    const prodItem = +item.textContent * quantities[i];
+    total.push(prodItem);
+  });
+
+  // Totals
+  const totalAmount = total.reduce((total, item) => {
+    total += item;
+    return total;
+  }, 0);
+
+  const totalQuantities = quantities.reduce((total, item) => {
+    total += +item;
+    return total;
+  }, 0);
+
+  // Formatted total amount
+  const finalAmount = totalAmount.toFixed(2);
+
+  // total amount in text
+  document.querySelector(".cart__total").textContent = "PHP " + finalAmount;
+  document.querySelector(".cart__notif").textContent = totalQuantities;
+
+  if (quantities.length !== 0) {
+    const wrapper0 = document.querySelector(".cart__details__wrapper--0");
+    const wrapper1 = document.querySelector(".cart__details__wrapper--1");
+
+    wrapper0.classList.add("hidden");
+    wrapper1.classList.add("show");
+  }
+}
+
+// IIFE
+(function () {
+  const cart = document.querySelector(".cart");
+
+  const cartInfo = document.querySelector(".cart__details");
+
+  cart.addEventListener("click", (e) => {
+    e.preventDefault();
+    cartInfo.classList.toggle("show");
+  });
+})();
+
+/*
+Things to add in JS Add to Cart feature.
+
+  1. Add a notification that the item is already added to the cart.
+
+  2. Create a navigation cart icon. - DONE
+
+  3. Option to remove items from the cart.
+
+  4. Computes the total amount of each items from the cart. - DONE
+
+  5. List of Products on cart shows when toggle -DONE
+
+  6. Total and Checkout button will be added once there is an item to the cart. - DONE
+
+  7. Add text change to the cart, whenever user increases or decreases the value.
+
+  8. Masked the text when the product name is too long.
+  
+
+*/
